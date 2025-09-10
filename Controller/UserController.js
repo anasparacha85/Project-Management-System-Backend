61abad81d953e459ef7452e279853590dbab7439
anasparacha85 <anasparacha85@gmail.com>
2025-09-10 19:56:36 +0500

    commit

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -48,20 +48,20 @@ const ok=await await user.comparePassword(password)
 
   // Cookie set karna
   //localhost
-  res.cookie('token', token, {
-    httpOnly: true,   // JS se access nahi hoga
-  secure: false,       // dev ke liye false, prod me true
-  sameSite: "lax",
-    maxAge: 24 * 60 * 60 * 1000, // 1 din
-  });
+  // res.cookie('token', token, {
+  //   httpOnly: true,   // JS se access nahi hoga
+  // secure: false,       // dev ke liye false, prod me true
+  // sameSite: "lax",
+  //   maxAge: 24 * 60 * 60 * 1000, // 1 din
+  // });
 
 //producttion
-//   res.cookie("token", token, {
-//   httpOnly: true,
-//   secure: process.env.NODE_ENV === "production", // prod me true
-//   sameSite: "None", // cross-site requests ke liye zaroori
-//   maxAge: 24 * 60 * 60 * 1000,
-// });
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", // prod me true
+  sameSite: "None", // cross-site requests ke liye zaroori
+  maxAge: 24 * 60 * 60 * 1000,
+});
 
   return res.json({SuccessMessage: 'Login successful' });
 };
@@ -85,19 +85,19 @@ const loginManager = async (req, res) => {
 
   // Cookie set karna
   //local host
-  res.cookie('token', token, {
-    httpOnly: true,   // JS se access nahi hoga
-   secure: false,       // dev ke liye false, prod me true
-  sameSite: "lax",
-    maxAge: 24 * 60 * 60 * 1000, // 1 din
-  });
+  // res.cookie('token', token, {
+  //   httpOnly: true,   // JS se access nahi hoga
+  //  secure: false,       // dev ke liye false, prod me true
+  // sameSite: "lax",
+  //   maxAge: 24 * 60 * 60 * 1000, // 1 din
+  // });
   //production
-//   res.cookie("token", token, {
-//   httpOnly: true,
-//   secure: process.env.NODE_ENV === "production", // prod me true
-//   sameSite: "None", // cross-site requests ke liye zaroori
-//   maxAge: 24 * 60 * 60 * 1000,
-// });
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", // prod me true
+  sameSite: "None", // cross-site requests ke liye zaroori
+  maxAge: 24 * 60 * 60 * 1000,
+});
 
   return res.json({ SuccessMessage: 'Login successful' });
 };
